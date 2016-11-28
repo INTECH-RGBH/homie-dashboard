d7da281210ecf445d613b5d32054ceb36b464f61
Valentin <beian77@hotmail.fr>
2016-11-28 15:46:26 +0100

    :sparkles: Add initial components

---
+++ app/components/devices/Switch.vue
@@ -0,0 +1,24 @@
+<template>
+  <card-device>
+    <div slot="img">
+      <img src="../../assets/images/icons/door/door-open.png" alt="" >
+    </div>
+    <div slot="main">
+        
+    </div>
+  </card-device>
+</template>
+
+<script>
+import CardDevice from "./Card"
+
+export default {
+  props: ['state', 'deviceId', 'nodeId'],
+
+  components:{CardDevice}
+}
+</script>
+
+<style lang="sass">
+
+</style>
--- app/components/pages/Devices.vue
+++ app/components/pages/Devices.vue
@@ -4,12 +4,46 @@
     <h2 class="subtitle">
       Tous les périphériques de votre maison
     </h2>
+    <div class="columns is-multiline">
+      <template v-for = "device in infrastructure">
+        <component v-for="node in device.nodes" :is="types[node.type]" :state = "node.properties" :deviceId = "device.id" :nodeId = "node.id"></component>
+      </template>
+
+    </div>
   </div>
 </template>
 
 <script>
+import {mapState} from "eva.js"
+
+import SwitchDevice from "../devices/Switch"
+import LightDevice from "../devices/Light"
+import TemperatureDevice from "../devices/Temperature"
+import HumidityDevice from "../devices/Humidity"
+import ShuttersDevice from "../devices/Shutters"
+import DoorDevice from "../devices/Door"
+import WindowDevice from "../devices/Window"
+import LockDevice from "../devices/Lock"
+import HeaterDevice from "../devices/Heater"
+import SoundDevice from "../devices/Sound"
+import LuminosityDevice from "../devices/Luminosity"
+import MotionDevice from "../devices/Motion"
+import BuzzerDevice from "../devices/Buzzer"
+
 export default {
+  data () {
+    return {
+      types: {
+        'temperature': TemperatureDevice,
+        'light': LightDevice
+      }
+    }
+  },
+  computed: {
+    ...mapState(["infrastructure"])
+  }
 }
+
 </script>
 
 <style lang="sass">
