84b9c556606d29c39ee6cf7f4a3305f2a2fb585d
Valentin <beian77@hotmail.fr>
2016-11-28 16:24:57 +0100

    Add method

--- app/components/devices/Temperature.vue
+++ app/components/devices/Temperature.vue
@@ -1,21 +1,33 @@
 <template>
   <card-device>
     <div slot="img">
-      <img src="../../assets/images/icons/door/door-open.png" alt="" >
+      <img src="../../assets/images/icons/temperature/thermometer.png" alt="" >
     </div>
     <div slot="main">
-      {{state.degrees.value}}
+      {{state.degrees.value}} 
     </div>
   </card-device>
 </template>
 
 <script>
 import CardDevice from "./Card"
+import {mapActions} from "eva.js"
 
 export default {
-  props: ['state', 'deviceId', 'nodeId'],
+  props: ['state', 'deviceId', 'nodeId', ],
 
-  components:{CardDevice}
+  components:{CardDevice},
+
+  methods:{
+    changeTemp(){
+      this.setState({
+            deviceId: this.deviceId,
+            nodeId: this.nodeId,
+            property: "",
+            value: "1"})
+    },
+    ...mapActions(["setState"])
+  }
 }
 </script>
 
