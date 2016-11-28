821415c14197b686d5188b431d7b02d9975d644d
Valentin <beian77@hotmail.fr>
2016-11-28 15:46:26 +0100

    :sparkles: Add initial components

---
+++ app/components/devices/Heater.vue
@@ -0,0 +1,24 @@
+<template>
+  <card-device>
+    <div slot="img">
+      <img src="../../assets/images/icons/door/door-open.png" alt="" >
+    </div>
+    <div slot="main">
+      {{state.degrees.value}}
+    </div>
+  </card-device>
+</template>
+
+<script>
+import CardDevice from "./Card"
+
+export default {
+  props: ['state', 'deviceId', 'nodeId'],
+
+  components:{CardDevice}
+}
+</script>
+
+<style lang="sass">
+
+</style>
