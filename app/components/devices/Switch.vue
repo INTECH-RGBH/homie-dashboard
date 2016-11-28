8678f2ea56e5ca2310ab14957ccc554adb0bf5cb
GautheyValentin <beian77@hotmail.fr>
2016-11-28 19:09:41 +0100

    Work on switch

--- app/components/devices/Switch.vue
+++ app/components/devices/Switch.vue
@@ -1,22 +1,36 @@
 <template>
   <card-device>
     <div slot="img">
-      
-      <img src="../../assets/images/icons/door/door-open.png" alt="" >
+
+      <img v-if="state.on.value === '1'" src="../../assets/images/icons/switch/switch-on.png" alt="" >
+      <img v-else-if="state.on.value === '0'" src="../../assets/images/icons/switch/switch-off.png" alt="" >
+      <img v-else src="../../assets/images/icons/common/unknown.png" alt="" >
     </div>
     <div slot="main">
-        
+        <button @click="turnSwitch">hello</button>
     </div>
   </card-device>
 </template>
 
 <script>
 import CardDevice from "./Card"
+import {mapActions} from "eva.js"
 
 export default {
   props: ['state', 'deviceId', 'nodeId'],
   components:{CardDevice},
- 
+  methods: {
+    turnSwitch(){
+      this.setState({
+            deviceId: this.deviceId,
+            nodeId: this.nodeId,
+            property: "on",
+            value: (this.state.on.value === '1' ? 0 : 1).toString()})
+      console.log(this.state.on.value === '1' ? 0 : 1)
+      console.log(this.state.on.value)
+    },
+    ...mapActions(["setState"])
+  }
 }
 </script>
 
--- app/components/devices/Temperature.vue
+++ app/components/devices/Temperature.vue
@@ -2,7 +2,7 @@
   <card-device>
     <div slot="img">
       <img src="../../assets/images/icons/temperature/thermometer.png" alt="" >
-      <canvas ref="canvas"></canvas>
+      <!--<canvas ref="canvas"></canvas>-->
     </div>
     <div slot="main">
       {{state.degrees ? state.degrees.value : ''}} 
@@ -19,26 +19,9 @@ export default {
 
   components:{CardDevice},
 
-  methods:{
-    changeTemp(){
-      this.setState({
-            deviceId: this.deviceId,
-            nodeId: this.nodeId,
-            property: "",
-            value: "1"})
-    },
-    ...mapActions(["setState"])
-  },
    mounted(){
     const canvas = this.$refs.canvas.getContext("2d")
-    console.log(canvas)
-    canvas.beginPath();//On démarre un nouveau tracé
-    canvas.moveTo(0, 300);//On se déplace au coin inférieur gauche
-    canvas.lineTo(150, 0);
-    canvas.lineTo(300, 300);
-    canvas.lineTo(0, 300);
-    canvas.stroke();//On trace seulement les lignes.
-    canvas.closePath();
+    
   }
 }
 </script>
