2a6a86fc8b6ad62429ecd5a089b54f4618be186b
Valentin <beian77@hotmail.fr>
2016-11-28 15:46:26 +0100

    :sparkles: Add initial components

---
+++ app/components/devices/Card.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="column is-one-quarter">
+<div class="card">
+  <div class="card-image">
+    <figure class="image is-256x256">
+
+      <slot name="img"></slot>
+
+
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+      </div>
+      <div class="media-content">
+        <p class="title is-5">
+          <slot name="main"></slot>
+        </p>
+      </div>
+    </div>
+
+    <div class="content">
+        <slot name="content"></slot>
+    </div>
+  </div>
+</div>
+</div>
+</template>
+<script>
+export default{
+
+}
+</script>
+
+<style lang="sass">
+    .card
+        margin-left: 10px
+        margin-top: 10px
+    
+</style>
