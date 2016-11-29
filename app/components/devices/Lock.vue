6e17fdb3c7eb5706fbce9988c2284b63ea845d61
Mathieu Boissady <boissady@intechinfo.fr>
2016-11-29 10:03:21 +0100

    blabla

--- app/components/devices/Lock.vue
+++ app/components/devices/Lock.vue
@@ -1,20 +1,44 @@
 <template>
   <card-device>
     <div slot="img">
-      <img src="../../assets/images/icons/door/door-open.png" alt="" >
+      <img v-if = "state.open === undefined" src = "../../assets/images/icons/common/unknown.png">
+      <img v-else-if = "state.open.value === '1'" src = "../../assets/images/icons/door/door-open.png" alt ="">
+      <img v-else src="../../assets/images/icons/door/door-close.png" alt="" >
     </div>
-    <div slot="main">
+    <div v-if = "state.open !== undefined" slot="main">
+      {{state.open.value}}
+       <button v-on:click="turnLock()">Modify</button>
     </div>
+    <div v-else slot = "main">
+      <button v-on:click="turnLock()">Modify</button>
+    </div>
+    
   </card-device>
 </template>
 
 <script>
 import CardDevice from "./Card"
+import {mapActions} from "eva.js"
 
 export default {
   props: ['state', 'deviceId', 'nodeId'],
 
-  components:{CardDevice}
+  components:{CardDevice},
+
+  methods : {
+    async turnLock() {
+      try{
+       await this.setState({
+            deviceId: this.deviceId,
+            nodeId: this.nodeId,
+            property: "open",
+            value: this.state.open.value === "1" ? "0" : "1"})
+      }
+      catch (e) {console.log("nique")}
+    },
+    ...mapActions(["setState"])
+  }
+  
 }
 </script>
 
