eb62aaba7250dd2c8c554d27de8d43007fb0d44d
Marvin Roger <nivramdu94@gmail.com>
2016-12-09 13:08:23 +0100

    :bug: Fix foreign key violation on tag delete

--- migrations/002-tags.sql
+++ migrations/002-tags.sql
@@ -13,9 +13,9 @@ CREATE TABLE nodes_tags (
   node_id INTEGER NOT NULL,
   tag_id TEXT NOT NULL,
 
-  CONSTRAINT nodes_tags_node_id_fk FOREIGN KEY(node_id) REFERENCES nodes(id),
-  CONSTRAINT nodes_tags_tag_id_fk FOREIGN KEY(tag_id) REFERENCES tags(id),
-  CONSTRAINT nodes_tags_node_per_tag_uq UNIQUE(node_id, tag_id) ON CONFLICT REPLACE
+  CONSTRAINT nodes_tags_node_id_fk FOREIGN KEY(node_id) REFERENCES nodes(id) ON DELETE CASCADE,
+  CONSTRAINT nodes_tags_tag_id_fk FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE,
+  CONSTRAINT nodes_tags_node_per_tag_uq UNIQUE(node_id, tag_id) ON CONFLICT IGNORE
 );
 
 --------------------------------------------------------------------------------
